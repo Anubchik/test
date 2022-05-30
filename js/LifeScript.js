e9ee7c179e9b602d342e7f52be222447140f88c4
Anubchik <mudzanvsmadara@gmail.com>
2022-05-30 12:02:09 +0500

    first

---
+++ js/LifeScript.js
@@ -0,0 +1,55 @@
+//
+// const name = 'Asil lox pashsha'
+// console.log(name)
+// alert(name)
+//
+// function exe(randomSource) {
+//     Math.random(min, max)
+// }
+//
+// const min = 10
+// const max = 20
+// console.log(exe)
+//
+// const meme = function () {
+//     Math.random(10, 20)
+// }
+//
+// console.log(meme)
+//
+// const number = 15
+// const name = 'Quvonch'
+// const lastyear = 2007
+// const year = 2023
+// const lastname = 'Sheri Meri'
+//
+// function exe() {
+//     if (year - lastyear === number) {
+//         console.log(name)
+//     } else if (year - lastyear < '' > number) {
+//         console.log(lastname)
+//     }
+// }
+//
+// console.log(exe)
+//
+// else if (year - lastyear > number) {
+//     console.log(lastname)
+// }
+// const name = 'Sherzod'
+// const lastname = 'Azimov'
+// const age = 14
+// const year = 2008
+// const lastyear = 2022
+//
+// if (lastyear - year === age) {
+//     alert(name)
+// }
+// min = 10
+// max = 20
+// const math = Math.random(min,max)
+// console.log(math)
+// console.log(last)
+// const last = isFinite(100)
+// const fox = Math.random(isFinite(-100,100))
+// console.log(fox)
